286316b92bba872aac8b8123ec5d627e4df51416
ashutoshpandey1710 <ashu1710@gmail.com>
2017-07-30 17:32:36 -0500

    Added ML Classifier (SVM).

--- core/classify.py
+++ core/classify.py
@@ -1,4 +1,9 @@
-from core.data import VectorColumns
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler
+from sklearn.svm import SVC
+
+from core.data import VectorColumns, get_dataset_as_dataframe, vectorize_dataset, vectorize_from_string
 import numpy as np
 
 class RuleBasedClassifier:
@@ -19,3 +24,38 @@ class RuleBasedClassifier:
                 else:
                     Y[i] = 0.0
             return Y
+
+class MLClassifier:
+    def __init__(self, datafile):
+        self.datafile = datafile
+        self.pipeline = Pipeline([
+            ('poly', PolynomialFeatures(2)),
+            ('scaler', StandardScaler()),
+            ('svm', SVC()),
+        ])
+        self.best_estimator = None
+        self.best_score = 0.0
+
+        self.params = {
+            'svm__C': (0.1, 0.5, 1., 5., 10.),
+            'svm__kernel' : ('linear', 'rbf', 'sigmoid')
+        }
+
+    def train_model(self):
+        print("Training Email Address Classifier using Grid Search and SVMs...")
+        X, Y = vectorize_dataset(get_dataset_as_dataframe(self.datafile))
+        grid_search = GridSearchCV(self.pipeline, self.params, n_jobs=2, verbose=1)
+        grid_search.fit(X, Y)
+
+        self.best_estimator = grid_search.best_estimator_
+        self.best_score = grid_search.best_score_
+        print("Done.")
+
+    def predict_spam(self, email_address):
+        X = vectorize_from_string(email_address)
+
+        if self.best_estimator:
+           return self.best_estimator.predict(X)
+        else:
+            print("MLClassifier.predict_spam: Model is not trained yet. Returning -1.")
+            return -1.0
