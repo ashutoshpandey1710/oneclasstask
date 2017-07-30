fe610522696a2b98d7c87892ced10cf8f5cb9085
ashutoshpandey1710 <ashu1710@gmail.com>
2017-07-29 22:05:12 -0500

    Fixed unit tests.

--- tests/data_test.py
+++ tests/data_test.py
@@ -12,20 +12,20 @@ class DataTest(TestCase):
     def setUp(self):
         print("Setting up DataTest...")
         self.datafile = "../test_dataset.csv"
-        self.df = pd.read_csv(self.datafile)
+        self.df = pd.read_csv(self.datafile, names=['email_address', 'is_spam'])
 
     def test_get_dataset_as_dataframe(self):
         df = get_dataset_as_dataframe(self.datafile)
 
         self.assertEqual(len(df), 9, "Length of df is {}. Should be 9.".format(len(df)))
-        self.assertMultiLineEqual(df.at[0, 'email_address'], "c75c752f0a5348820b19@gmail.com", "First email address does not match.")
+        self.assertMultiLineEqual(df.at[0, 'email_address'], "b19@gmail.com", "First email address does not match.")
 
     def test_has_at_sign(self):
         positive_case = "e9b0f521a11ecd1ff560@gmail.com"
         negative_case = "gmail.com"
 
         self.assertTrue(has_at_sign(positive_case), "has_at_sign failed for string {}".format(positive_case))
-        self.assertFalse(negative_case, "has_at_sign failed for string {}".format(negative_case))
+        self.assertFalse(has_at_sign(negative_case), "has_at_sign failed for string {}".format(negative_case))
 
     def tear_pre_at_length(self):
         short = pre_at_length("f@gmail.com")
@@ -46,4 +46,3 @@ class DataTest(TestCase):
         self.assertAlmostEqual(Y[5], 1., places=4)
         self.assertAlmostEqual(Y[6], 0., places=4)
         self.assertAlmostEqual(Y[7], 1., places=4)
-        self.assertAlmostEqual(Y[9], 0., places=4)
